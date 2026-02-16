4fb97ce9b7b00ec5955e90b75cfaa348d72e2898
Dehedero <43905800+Dehedero@users.noreply.github.com>
2026-02-17 02:00:49 +0300

    I like to move it move it + on screen objects

---
+++ root/Scripts/on_screen_object/onscreen_object_state.gd
@@ -0,0 +1,22 @@
+class_name OnScreenObjectState
+extends Area2D
+
+@export var transitions : Array[ObjectStateTransition]
+@export var to_set_true: Array[String]
+@export var to_set_false: Array[String]
+
+func go_to(new_state : OnScreenObjectState) ->  void:
+	for true_flag in to_set_true:
+		ProgressionManager.set_flag(true_flag, true)
+	for false_flag in to_set_false:
+		ProgressionManager.set_flag(false_flag, false)
+	self.visible = false
+	new_state.visible = true
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+		for transition in transitions:
+			var new_state = transition.go_to_new_state()
+			if new_state:
+				return
