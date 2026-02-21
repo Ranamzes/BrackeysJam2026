7fbb0d12e7236357b5f1d86d2609d4a300062b40
Eugene <arhar18rus@gmail.com>
2026-02-21 15:06:13 -0800

    merge

--- root/scenes/levels/level1/level_1_center.gd
+++ root/scenes/levels/level1/level_1_center.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ProgressionManager.set_flag("level_1_visited", true)
+	ProgressionManager.progression_data.changed.connect(flags_changed)
+
+func flags_changed(flag_name : String, flag_value : bool) -> void:	
+	match flag_name:
+		"soap_full_picked_up":
+			GlobalData.player_inventory.remove_item(preload("res://root/assets/items/soap_empty.tres"))
+			GlobalData.player_inventory.add_item(preload("res://root/assets/items/soap_full.tres"))
