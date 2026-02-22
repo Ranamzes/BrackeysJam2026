1e2d08853a103194270d616d287569bfbdbd49bf
Dehedero <43905800+Dehedero@users.noreply.github.com>
2026-02-22 12:29:12 +0300

    + sounds

--- root/scenes/UI/elements/draggable_object/draggable_object.gd
+++ root/scenes/UI/elements/draggable_object/draggable_object.gd
@@ -4,6 +4,8 @@ extends PanelContainer
 @export var shampoo_texture : TextureRect
 @export var id : String
 @export var is_empty : bool = false
+@export var audios : Array[AudioStream]
+
 var shampoo_game : ShampooGame 
 var def_position : Vector2
 var clicks_disabled : bool = false
@@ -65,5 +67,7 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 		shampoo_game.dragging_shampoo.shampoo_texture.modulate = Color.WHITE
 	else:
 		shampoo_texture.modulate = Color.WHITE
+	if ! audios.is_empty() :
+		AudioService.play_sound(audios, &"SFX", [-12, -8], [0.5, 1.2])
 	shampoo_game.check_solved()
 	
