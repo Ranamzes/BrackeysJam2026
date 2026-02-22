1fa6588aed4d33f3faf49829ed872eadc38365c1
Eugene <arhar18rus@gmail.com>
2026-02-21 21:51:17 -0800

    TypeMachine

--- root/scenes/minigames/typing_machine/pointer.gd
+++ root/scenes/minigames/typing_machine/pointer.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
 
+class_name Pointer
 @export var movement_component:MovementComponent
 @export var path_size:int = 700
 @export var speed_multiplier: float = 5
+@onready var area: Area2D = %Area2D
+@onready var sprite:Sprite2D = $Sprite2D
+@export var textures:Array[Texture2D]
+var base_speed = 40
+var current_speed
 var global_top_boundary:Vector2
 var global_bottom_boundary:Vector2
-var is_going_top = false
+var is_going_top = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	movement_component.base_speed = movement_component.base_speed * speed_multiplier
+	current_speed = 7
 	global_top_boundary = global_position
 	global_bottom_boundary = global_position
 	global_top_boundary.y = global_top_boundary.y - path_size/2.0
 	global_bottom_boundary.y = global_bottom_boundary.y + path_size/2.0
+	global_position.y =  global_position.y - path_size/4.0
+	print(movement_component.base_speed)
 
+	modulate.a = 0
+	reveal_pointer()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_going_top:
@@ -30,6 +41,20 @@ func _process(delta: float) -> void:
 	
 	movement_component.move(self)
 	
-	print(global_position.y)
-	print(global_top_boundary.y)
-	print(global_position)
+func reveal_pointer() -> void:
+	update_speed(current_speed)
+	var tween = create_tween()
+	tween.tween_property(self ,"modulate:a",255,0.35).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	
+	
+func update_speed(speed_mult:int)->void:
+	print(base_speed * speed_mult)
+	movement_component.base_speed =base_speed * speed_mult
+
+func hide_pointer(next_speed:int) -> void:
+	current_speed = next_speed
+	var tween = create_tween()
+	tween.tween_property(self ,"modulate:a",0,0.35).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+
+func change_texture(index:int)->void:
+	sprite.texture=textures[index]
--- root/scenes/minigames/typing_machine/typing_machine.gd
+++ root/scenes/minigames/typing_machine/typing_machine.gd
@@ -1,11 +1,55 @@
 extends Node2D
 
-
+@onready var trigger_zone:Area2D = %TriggerZone
+@onready var pointer:Pointer = %Pointer
+@onready var timer:Timer = %Timer
+@onready var success_audio: StreamPlayerComponent = $SuccessAudio
+@onready var fail_audio: StreamPlayerComponent = $FailAudio
+@onready var end_audio: StreamPlayerComponent = $EndAudio
+@export var letters: Array[Sprite2D]
+var current_level = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	timer.timeout.connect(on_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if (event as InputEventMouseButton).button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			timer.start(1.5)
+			if(trigger_zone.get_overlapping_areas().size()>0):
+				if(current_level<5):
+					key_down()
+					success_audio.play_random()
+					letters[current_level].visible=true
+				current_level += 1
+			else:
+				if(current_level<5):
+					fail_audio.play_random()	
+		
+			pointer.hide_pointer(5+6*current_level)
+
+func on_timeout():
+	if current_level<5:
+		pointer.change_texture(current_level)
+		pointer.reveal_pointer() 
+	else:
+		$GameZone.visible = false
+		end_audio.play_random()
+		ProgressionManager.set_flag("typing_solved",true)
+
+func key_down():
+	var keys = get_tree().get_nodes_in_group("keys")
+	var rand = RandomNumberGenerator.new()
+	rand.randomize()
+	var key:Sprite2D = keys[rand.randi_range(0,keys.size()-1)]
+	var tween = create_tween()
+	tween.tween_property(key ,"position:y",key.position.y+15,0.35)
+	tween.tween_callback(key_up.bind(key))
+	
+func key_up(key:Sprite2D):
+	var tween = create_tween()
+	tween.tween_property(key ,"position:y",key.position.y-15,0.35)
