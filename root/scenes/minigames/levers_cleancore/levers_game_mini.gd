21b182ec4237e2a04a380b7cdc84fb38c5a40632
Eugene <arhar18rus@gmail.com>
2026-02-21 23:13:10 -0800

    fix levers and level2

--- root/scenes/minigames/levers_cleancore/levers_game_mini.gd
+++ root/scenes/minigames/levers_cleancore/levers_game_mini.gd
@@ -4,17 +4,22 @@ extends Node2D
 @export var solved_flag_name : String = "levers_solved"
 
 func _ready() -> void:
-	ProgressionManager.progression_data.changed.connect(_on_changed)
+	ProgressionManager.flag_changed.connect(_on_changed)
+	
 
 func _on_changed(flag_name : String, flag_value : bool) -> void:
+	print(flag_name)
+	print(levers_check_state)
 	if levers_check_state.has(flag_name) :
 		print(flag_name)
-		if check_solved() :
+		if check_solved() and !ProgressionManager.get_flag(solved_flag_name):
 			ProgressionManager.set_flag(solved_flag_name, true)
 
 
 func check_solved() -> bool:
+	print("debug")
 	for key in levers_check_state :
+		print(levers_check_state[key])
 		if levers_check_state[key] != ProgressionManager.get_flag(key) :
 			return false
 	return true
