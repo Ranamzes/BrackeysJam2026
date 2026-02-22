408c2c3e539b7987da61b1dd05013a782282b577
Dehedero <43905800+Dehedero@users.noreply.github.com>
2026-02-22 14:47:31 +0300

    puzzle restote fixed

--- root/scripts/puzzle/puzzle_piece_disabler.gd
+++ root/scripts/puzzle/puzzle_piece_disabler.gd
@@ -5,18 +5,20 @@ var puzzle_piece : PuzzlePiece
 @export var required_item : ItemData
 @export var required_item_id : String
 @export var required_flags : Array[String]
+@export var state_flag_name : String
 
 func _ready() -> void:
 	puzzle_piece = get_parent()
-	puzzle_piece.disable()
+	if !ProgressionManager.get_flag(state_flag_name) :
+		puzzle_piece.disable()
 	self.input_event.connect(_on_input_event)
 	
 func _on_input_event(viewport : Node, event : InputEvent, shape_idx : int) -> void:
-	if event is InputEventMouseButton :
+	if !puzzle_piece.is_enabled && event is InputEventMouseButton :
 		event = event as InputEventMouseButton
 		if event.button_index == MOUSE_BUTTON_LEFT && event.is_pressed() :
 			if !required_item_id || (GlobalData.selected_slot && GlobalData.selected_slot.inventory_slot.item.id == required_item_id) :
 				if ProgressionManager.check_flags(required_flags) :
 					puzzle_piece.enable()
 					GlobalData.player_inventory.remove_item(required_item)
-					ProgressionManager.set_flag("frame_placed",true)
+					ProgressionManager.set_flag(state_flag_name, true)
